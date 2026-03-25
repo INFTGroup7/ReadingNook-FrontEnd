4a3f3dccbd9498a853905b5a41901bbf98001a69
INFTGroup7 <inftgroup7@outlook.com>
2026-03-25 18:56:27 -0400

    Update page.jsx

--- src/app/books/list/page.jsx
+++ src/app/books/list/page.jsx
@@ -4,7 +4,7 @@ import BookList from './BookList';
 
 export default async function BookListPage() {
 
-  const response = await fetch(`http://backend-balancer-496462085.us-east-1.elb.amazonaws.com:8080/books`, {
+  const response = await fetch(`http://backend-balancer-963158454.ap-east-1.elb.amazonaws.com:8080/books`, {
 
     cache: 'no-store',
   });
